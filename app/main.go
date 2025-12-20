1d915905d63825adf3a5f6414dff9eda9cc74a42
imeltgp <2827228085@qq.com>
2025-12-20 21:52:36 +0800

    [any message]

--- app/main.go
+++ app/main.go
@@ -21,9 +21,21 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
+	defer func(conn net.Conn) {
+		err := conn.Close()
+		if err != nil {
+			os.Exit(1)
+		}
+	}(conn)
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
+
+	_, err = conn.Write([]byte("+PONG\r\n"))
+	if err != nil {
+		// handle error
 		os.Exit(1)
 	}
 	_ = l.Close()
