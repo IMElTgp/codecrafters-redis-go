1955de9766de67d423adc5f3dcf1f712dd9aa011
imeltgp <2827228085@qq.com>
2025-12-21 23:13:35 +0800

    test

--- app/main.go
+++ app/main.go
@@ -33,13 +33,13 @@ func main() {
 		}
 	}()
 
-	for {
-		conn, err := l.Accept()
-		if err != nil {
-			fmt.Println("Error accepting connection: ", err.Error())
-			os.Exit(1)
-		}
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println("Error accepting connection: ", err.Error())
+		os.Exit(1)
+	}
 
+	for {
 		_, err = conn.Write([]byte("+PONG\r\n"))
 		if err != nil {
 			// handle error
