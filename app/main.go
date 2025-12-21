d1f314131a825d4c6ab07810b0be65c5a157e52d
imeltgp <2827228085@qq.com>
2025-12-21 23:09:04 +0800

    [test]

--- app/main.go
+++ app/main.go
@@ -6,6 +6,10 @@ import (
 	"os"
 )
 
+// redis-written-in-go
+// Golang is perfect
+// IDK what the fuck Redis actually is
+
 // Ensures gofmt doesn't remove the "net" and "os" imports in stage 1 (feel free to remove this!)
 var _ = net.Listen
 var _ = os.Exit
@@ -15,28 +19,36 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	// Uncomment the code below to pass the first stage
-	//
+
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	defer func(conn net.Conn) {
-		err := conn.Close()
+	defer func() {
+		err = l.Close()
 		if err != nil {
+			// handle error
 			os.Exit(1)
 		}
-	}(conn)
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
+	}()
 
-	_, err = conn.Write([]byte("+PONG\r\n"))
-	if err != nil {
-		// handle error
-		os.Exit(1)
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		_, err = conn.Write([]byte("+PONG\r\n"))
+		if err != nil {
+			// handle error
+			err = conn.Close()
+			if err != nil {
+				// handle error
+				return
+			}
+			return
+		}
 	}
-	_ = l.Close()
 }
---
+++ note.md
@@ -0,0 +1,7 @@
+## Redis-Go Challenge
+
+### Intro
+
+#### Respond to multiple PINGs
+
+在无限循环中读取client发来的命令并发回respond。
