d1b6d9af54fc7c29b952012cac84844f94f193f8
imeltgp <2827228085@qq.com>
2026-02-15 19:36:19 +0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -262,7 +262,7 @@ func (c *Conn) runLRANGE(args []string) error {
 }
 
 func (c *Conn) runLLEN(args []string) error {
-	if len(args) != 2 {
+	if len(args) != 1 {
 		return fmt.Errorf("LLEN: argument count mismatch")
 	}
 
