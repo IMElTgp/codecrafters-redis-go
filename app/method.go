a04371379f8f5122981e8d341bb5f3ec89e641e8
imeltgp <2827228085@qq.com>
2026-02-25 15:52:47 +0800

    test

--- app/method.go
+++ app/method.go
@@ -963,9 +963,8 @@ func (c *Conn) runINFO(args []string) error {
 }
 
 func (c *Conn) runREPLCONF(args []string) error {
-	if len(args) == 2 && args[1] == "*" {
-		// REPLCONF GETACK *
-		_, err := c.Conn.Write([]byte("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"))
+	if len(args) == 2 && strings.ToUpper(args[0]) == "GETACK" && args[1] == "*" {
+		_, err := c.write([]byte("*3\r\n$8\r\nREPLCONF\r\n$3\r\nACK\r\n$1\r\n0\r\n"))
 		return err
 	}
 
