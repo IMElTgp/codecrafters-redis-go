8fd29a7da9a1282eebbc23d8d920d1a2662f8dac
imeltgp <2827228085@qq.com>
2026-02-25 18:33:24 +0800

    test

--- app/handshake.go
+++ app/handshake.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"strconv"
+	"strings"
+)
+
 /**
  * handshake.go
  * handles handshake that happens between replica and master
@@ -49,12 +56,96 @@ func readFromMaster(c *Conn) error {
 	if serverRole {
 		return nil
 	}
-	buffer := make([]byte, 1024)
-	_, err := c.Conn.Read(buffer)
+	kind, line, err := readRESPValue(c.Conn)
 	if err != nil {
-		// handle error
 		return err
 	}
-
+	// After PSYNC, master sends: +FULLRESYNC ...\r\n then an RDB bulk string.
+	if kind == '+' && strings.HasPrefix(line, "FULLRESYNC") {
+		_, _, err = readRESPValue(c.Conn)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
+}
+
+func readRESPValue(conn io.Reader) (kind byte, line string, err error) {
+	b, err := readByte(conn)
+	if err != nil {
+		return 0, "", err
+	}
+
+	switch b {
+	case '+', '-', ':':
+		line, err = readLine(conn)
+		return b, line, err
+	case '$':
+		line, err = readLine(conn)
+		if err != nil {
+			return b, "", err
+		}
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			return b, "", err
+		}
+		if n == -1 {
+			return b, "", nil
+		}
+		// bulk string payload + \r\n
+		if err := discardN(conn, n+2); err != nil {
+			return b, "", err
+		}
+		return b, "", nil
+	case '*':
+		line, err = readLine(conn)
+		if err != nil {
+			return b, "", err
+		}
+		n, err := strconv.Atoi(line)
+		if err != nil {
+			return b, "", err
+		}
+		if n <= 0 {
+			return b, "", nil
+		}
+		for i := 0; i < n; i++ {
+			if _, _, err = readRESPValue(conn); err != nil {
+				return b, "", err
+			}
+		}
+		return b, "", nil
+	default:
+		return b, "", fmt.Errorf("unexpected RESP type byte: %q", b)
+	}
+}
+
+func readByte(conn io.Reader) (byte, error) {
+	var buf [1]byte
+	_, err := io.ReadFull(conn, buf[:])
+	return buf[0], err
+}
+
+func readLine(conn io.Reader) (string, error) {
+	var b strings.Builder
+	for {
+		ch, err := readByte(conn)
+		if err != nil {
+			return "", err
+		}
+		if ch == '\n' {
+			break
+		}
+		b.WriteByte(ch)
+	}
+	line := b.String()
+	return strings.TrimSuffix(line, "\r"), nil
+}
+
+func discardN(conn io.Reader, n int) error {
+	if n <= 0 {
+		return nil
+	}
+	_, err := io.CopyN(io.Discard, conn, int64(n))
+	return err
 }
