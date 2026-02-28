89618041c53c8aa3b8e842bf243dbe85c47cb36f
imeltgp <2827228085@qq.com>
2026-03-01 00:23:50 +0800

    test

--- app/conn.go
+++ app/conn.go
@@ -383,6 +383,18 @@ func handleConn(c *Conn) {
 				if c.runPSYNC(args[1:]) != nil {
 					return
 				}
+			case "WAIT":
+				if multi {
+					cmdQueue, err = c.processMULTI(cmdQueue, args)
+					if err != nil {
+						// handle error
+						return
+					}
+					goto skip
+				}
+				if c.runWAIT(args[1:]) != nil {
+					return
+				}
 			}
 			// do propagation
 			// this should NOT be put inside `skip` label
