24cc23851b0447152efa4427376a364b558a7611
imeltgp <2827228085@qq.com>
2026-03-01 13:48:04 +0800

    codecrafters submit [skip ci]

--- app/conn.go
+++ app/conn.go
@@ -424,6 +424,18 @@ func handleConn(c *Conn) {
 				if c.runWAIT(args[1:]) != nil {
 					return
 				}
+			case "CONFIG":
+				if multi {
+					cmdQueue, err = c.processMULTI(cmdQueue, args)
+					if err != nil {
+						// handle error
+						return
+					}
+					goto skip
+				}
+				if c.runCONFIG(args[1:]) != nil {
+					return
+				}
 			}
 			// do propagation
 			// this should NOT be put inside `skip` label
