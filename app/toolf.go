a4cf7655bc3dfd860328b663cf39a2ba2c525c9c
imeltgp <2827228085@qq.com>
2026-03-01 12:45:10 +0800

    what the hell

--- app/toolf.go
+++ app/toolf.go
@@ -202,13 +202,11 @@ func parseArgs(msg string) (args []string, consumed int, err error) {
 
 // countAcked returns count of replicas that meet the demand of `WAIT`
 func countAcked(target int64) (count int) {
-	mu.Lock()
 	for repConn := range replicaConns {
 		if replicaOffset[repConn] >= target {
 			count++
 		}
 	}
-	mu.Unlock()
 	return
 }
 
