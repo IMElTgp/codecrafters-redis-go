e891b2d3ddc0d4c3f8000187a571780422d18738
imeltgp <2827228085@qq.com>
2026-03-04 11:36:13 +0800

    test for codecrafters

--- app/conn.go
+++ app/conn.go
@@ -510,6 +510,18 @@ func handleConn(c *Conn) {
 				if c.runZADD(args[1:]) != nil {
 					return
 				}
+			case "ZRANK":
+				if multi {
+					cmdQueue, err = c.processMULTI(cmdQueue, args)
+					if err != nil {
+						// handle error
+						return
+					}
+					goto skip
+				}
+				if c.runZRANK(args[1:]) != nil {
+					return
+				}
 			}
 			// do propagation
 			// this should NOT be put inside `skip` label
